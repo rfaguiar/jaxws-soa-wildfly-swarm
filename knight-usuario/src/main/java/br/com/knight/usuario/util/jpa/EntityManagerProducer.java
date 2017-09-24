3471c162d973a70b901ac8b20786a085980c81ae
Rogerio Aguiar <rfaguiar1@gmail.com>
2017-09-24 11:30:33 -0300

    Criado novo projeto de usuarios para realizar autenticação usando rest

---
+++ knight-usuario/src/main/java/br/com/knight/usuario/util/jpa/EntityManagerProducer.java
@@ -0,0 +1,40 @@
+package br.com.knight.usuario.util.jpa;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * @author rogerio
+ *
+ */
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	/*
+	@PersistenceContext
+	@Produces
+	private EntityManager manager;*/
+	
+
+	@PersistenceUnit
+    private EntityManagerFactory entityManagerFactory;
+
+    @Produces
+    @Default
+    @RequestScoped
+    public EntityManager create() {
+        return this.entityManagerFactory.createEntityManager();
+    }
+
+    public void dispose(@Disposes @Default EntityManager entityManager) {
+        if (entityManager.isOpen()) {
+            entityManager.close();
+        }
+    }
+}
